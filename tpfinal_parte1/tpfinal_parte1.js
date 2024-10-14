58e595467e9bca9fef44283481d1d87b5fca8708
lucilatenutto <130100427+lucilatenutto@users.noreply.github.com>
2024-10-14 11:51:32 -0300

    Add files via upload

--- tpfinal_parte1/tpfinal_parte1.js
+++ tpfinal_parte1/tpfinal_parte1.js
@@ -1,12 +1,25 @@
 let pantalla = 0; // esto controla la pantalla actual
 let imagenes = []; 
-
+let tiempoCambio = 5000; // 5 segundos 
+let tiempoUltimoCambio;
 
 function setup() {
-createCnvas(640, 480);
+createCanvas(640, 480);
+
+ // Inicializar el tiempo del último cambio de pantalla
+  tiempoUltimoCambio = millis();
 }
 
 
 function draw() {
-image(imagenes, 0, 0, width, height);
+background(220);
+
+if (millis() - tiempoUltimoCambio > tiempoCambio) {
+    pantalla++; // Avanzar a la siguiente pantalla
+    if (pantalla >= imagenes.length) {
+      pantalla = 0; // Reiniciar al menú principal después de la última imagen
+    }
+    tiempoUltimoCambio = millis(); // Actualizar el tiempo del último cambio
+  }
+   image(imagenes[pantalla], 0, 0, width, height);
 }
